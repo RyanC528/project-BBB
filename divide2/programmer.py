5dfed2bf00e5134507164343a43f7ddfbea1dcd5
ryanc <Ryan_Copeland@student.uml.edu>
2024-04-28 02:25:11 -0400

    thats alot of if statements

--- divide2/programmer.py
+++ divide2/programmer.py
@@ -1,16 +1,21 @@
-print("#include <stdio.h>")
-print("#include <stdint.h>")
-print("#include <stdlib.h>")
+f = open("divide2.c","w")
 
-print("int main(int argc, char* argv[])")
-print("{")
-print("    uint8_t number = atoi(argv[1]); // No problems here")
 
-for i in range(2**8):
-    print("    if (number == "+str(i)+")")
+f.write("#include <stdio.h>\n")
+f.write("#include <stdint.h>\n")
+f.write("#include <stdlib.h>\n\n")
+
+f.write("int main(int argc, char* argv[])")
+f.write("{\n")
+f.write("    uint8_t number = atoi(argv[1]); // No problems here\n\n")
+
+for i in range(2**16):
+    f.write("    if (number == "+str(i)+")\n")
     if i % 2 == 0:
-        print("        printf(\"even\\n\");")
+        f.write("        printf(\"even\\n\");\n")
     else:
-        print("        printf(\"odd\\n\");")
+        f.write("        printf(\"odd\\n\");\n")
 
-print("}")
+f.write("}")
+
+f.close
