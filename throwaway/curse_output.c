12d96d2825584287d68776e9449044508f299aa8
ryanc <Ryan_Copeland@student.uml.edu>
2024-04-30 23:56:53 -0400

    idk what to do with this

---
+++ throwaway/curse_output.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    int arr[5] = {1, 2, 3, 4, 5};
+    for (int i = 0; i < 5; i++)
+    {
+        printf("%d ", i[arr]); // wat
+    }
+    printf("\n");
+    return 0;
+}
